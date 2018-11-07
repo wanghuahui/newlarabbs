9427effb0278656b2e314dc30643741e310a30da
wanghuahui <wanghuahui.1112@163.com>
2018-11-07 15:20:57 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,12 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        // 空的时候才指定默认头像
+        if (empty($user->avatar)) {
+            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
