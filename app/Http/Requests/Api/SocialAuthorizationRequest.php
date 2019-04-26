29cc6a9419199fde5844d9ffacf0884f337040ee
wanghuahui <wanghuahui.1112@163.com>
2019-04-26 18:53:15 +0800

    social authorizations

--- app/Http/Requests/Api/SocialAuthorizationRequest.php
+++ app/Http/Requests/Api/SocialAuthorizationRequest.php
@@ -23,8 +23,15 @@ class SocialAuthorizationRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
+        $rules = [
+            'code' => 'required_without:access_token|string',
+            'access_token' => 'required_without:code|string',
         ];
+
+        if ($this->social_type == 'weixin' && !$this->code) {
+            $rules['openid'] = 'required|string';
+        }
+        
+        return $rules;
     }
 }
